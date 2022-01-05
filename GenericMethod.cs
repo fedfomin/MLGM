3004fe88e445a90950beffb3df78d15f46f181de
Fede Fomin <wffomin@protonmail.com>
2022-01-05 19:37:16 +0100

    Updates

--- GenericMethod.cs
+++ GenericMethod.cs
@@ -8,14 +8,43 @@ namespace Library
 {
     public static class GenericMethod
     {
+        /// <summary>
+        /// View all the properties of a class of type T
+        /// </summary>
+
         public static void ViewProperties<T>(T obj) where T : class, new()
         {
             List<string> properties = obj.GetType().GetProperties().Select(p => p.Name).ToList();
 
-            for (int i = 0; i < properties.Count; i++)
+            foreach (var item in properties)
             {
-                Console.WriteLine($"Field {i}: {properties[i]}"); // View properties of a class of type T
+                Console.WriteLine($"Field: {item}");
             }
+        }
+
+        /// <summary>
+        /// Create an instance of a list.
+        /// Example usage: List<People> listPeople = Add.ListInstance<People>();
+        /// </summary>
+
+        public static List<T> ListInstance<T>() where T : class, new()
+        {
+            Type listType = GetGenerciListType<T>();
+            List<T> list = (List<T>)Activator.CreateInstance(listType);
+            list.Add(new T { });
+
+            return list;
+        }
+
+        private static Type GetGenerciListType<T>()
+        {
+            Type objTyp = typeof(T);
+            var defaultListType = typeof(List<>);
+
+            Type[] itemTypes = { objTyp };
+            Type listType = defaultListType.MakeGenericType(itemTypes);
+
+            return listType;
         }
     }
 }
