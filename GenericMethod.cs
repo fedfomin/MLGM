d557ee83964302c291bf0a6717184b3da0bfd103
Fede Fomin <wffomin@protonmail.com>
2022-01-05 17:25:10 +0100

    Adding GenericMethod class that contains a ViewProperties of a class.

---
+++ GenericMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class GenericMethod
+    {
+        public static void ViewProperties<T>(T obj) where T : class, new()
+        {
+            List<string> properties = obj.GetType().GetProperties().Select(p => p.Name).ToList();
+
+            for (int i = 0; i < properties.Count; i++)
+            {
+                Console.WriteLine($"Field {i}: {properties[i]}"); // View properties of a class of type T
+            }
+        }
+    }
+}
